fedfe9b123f61b05d4ad476731f70639840a6aa9
Artyom <artyom.fayzulin@gmail.com>
2019-06-01 13:20:19 +0300

    first step - add staff

---
+++ src/components/TodoItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+    return (
+            <p style={{color: 'red'}}>Choose:
+                <input type="checkbox" />
+            </p>
+    );
+}
+
+export default App;
